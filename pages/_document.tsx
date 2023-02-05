bddc97978a0b2be15b1ba5a254da85940fd375b7
Rafael Lins <rafael.lins777@gmail.com>
2023-02-05 10:37:55 -0300

    SEO

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,18 @@ import {Head, Html, Main, NextScript} from "next/document"
 
 const Document = () =>
     <Html lang="en">
-        <Head/>
+        <Head>
+            <meta charSet="utf-8"/>
+            <link rel="shortcut icon" href="/favicon.ico"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+            <meta name="robots" content="index,follow"/>
+            <meta property="og:type" content="article"/>
+            <meta property="og:title" content="QRCode do Pix: Gere um QRCode para receber Pix no seu negócio"/>
+            <meta property="og:site_name" content="QRCode do Pix"/>
+            <meta property="og:url" content="https://qrcodedopix.com.br"/>
+            <meta property="og:image" content="/images/logo-pix-512.png"/>
+            <link rel="manifest" href="/site.webmanifest"/>
+        </Head>
         <body
             className="min-h-screen bg-white font-sans text-slate-900 antialiased dark:bg-slate-900 dark:text-slate-50">
         <Main/>
